ff207b3bc97680d7c6682832d97d6719642b4bf1
Baktybeks <baktybeks@gmail.com>
2023-01-10 04:54:42 +0600

    dz_7 finish

---
+++ dz_7/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PostsPage from "./pages/postsPage/PostsPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <PostsPage/>
+        </div>
+    );
+}
+
+export default App;
